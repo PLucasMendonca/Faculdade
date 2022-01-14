68d0f95a010536ecc879d5b8c02871947b0a2b3b
Lucas Mendonça <p.lucas.santos.mendonca@gmail.com>
2022-01-14 20:56:25 -0300

    Teste2

--- 1°semestre/programas-em-c/pratica04/lucas.c
+++ 1°semestre/programas-em-c/pratica04/lucas.c
@@ -19,4 +19,44 @@ for(i=0; i<10; i++){
 }
 if(c==10){printf("\n Não existe posições com o valor do elemento M!");} else{};
   return 0;
+} 
+#include <stdio.h>
+#define LIM 11
+
+int main(){
+float notas[LIM],soma=0.0, media;
+int i=0;
+
+for (i=1; i<LIM; i++) {
+printf("Digite a nota do aluno %d: ",i);
+scanf("%f",&notas[i]);
+soma+=notas[i];
+}
+media = soma/LIM;
+printf("\n A media dos alunos é de : %.2f \n",media);
+for (i=1; i<LIM; i++) {
+if (notas[i]>= media)
+printf("O aluno %d tem nota %.2f maior que a media.\n",i,notas[i]);
+}
+return 0;
+}
+#include <stdio.h>
+#define LIM 11
+
+int main(){
+float notas[LIM],soma=0.0, media;
+int i=0;
+
+for (i=1; i<LIM; i++) {
+printf("Digite a nota do aluno %d: ",i);
+scanf("%f",&notas[i]);
+soma+=notas[i];
+}
+media = soma/LIM;
+printf("\n A media dos alunos é de : %.2f \n",media);
+for (i=1; i<LIM; i++) {
+if (notas[i]>= media)
+printf("O aluno %d tem nota %.2f maior que a media.\n",i,notas[i]);
+}
+return 0;
 }
